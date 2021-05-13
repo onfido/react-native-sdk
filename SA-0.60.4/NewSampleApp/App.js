57893ddcab1dae14a249b42109d5fa0aece6b0a2
Luis Santana <luis.santana@onfido.com>
2021-05-12 17:23:07 -0700

    Add SampleApp App.js file and make changes for easier testing

--- SA-0.60.4/NewSampleApp/App.js
+++ SA-0.60.4/NewSampleApp/App.js
@@ -1,114 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react';
+import {Button, StyleSheet, Text, View, Platform} from 'react-native';
+import {Onfido, OnfidoCaptureType} from '@onfido/react-native-sdk';
+import createSdkToken from './backend-server-example';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends Component {
+  state = {
+    title: 'Welcome to Onfido React Native SDK!',
+    subtitle: "To get started, press 'Launch'",
+    status: 'Starting',
+    message: '--',
+    sdkToken: null,
+    sdkFlowComplete: false,
+  };
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  componentDidMount() {
+    const applicationId =
+      Platform.OS === 'ios'
+        ? 'org.reactjs.native.example.NewSampleApp'
+        : 'com.newsampleapp';
+    const applicant = {
+      first_name: 'Jane',
+      last_name: 'Doe',
+    };
+    this.getSDKToken(applicant, applicationId);
+  }
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+  getSDKToken = async (applicant, applicationId) => {
+    const newState = await createSdkToken(applicant, applicationId);
+    this.setState(newState);
+  };
+
+  startSDK = () => {
+    Onfido.start({
+      sdkToken: this.state.sdkToken,
+      localisation: {
+        ios_strings_file_name: 'Localizable',
+      },
+      flowSteps: {
+        welcome: true,
+        captureDocument: {},
+        captureFace: {
+          type: OnfidoCaptureType.VIDEO,
+        },
+      },
+    })
+      .then(response => {
+        this.setState({
+          status: 'resolved',
+          message: JSON.stringify(response),
+          sdkFlowComplete: true,
+        });
+      })
+      .catch(error => {
+        this.setState({
+          status: 'rejected',
+          message: error.code + ': ' + error.message,
+          sdkFlowComplete: true,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{this.state.title}</Text>
+        <Text style={styles.instructions}>{this.state.subtitle}</Text>
+        <Button title="Launch" onPress={() => this.startSDK()} />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 10,
   },
 });
-
-export default App;
