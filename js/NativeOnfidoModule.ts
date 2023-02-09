880edaef9820dc5809700580471151bc4bda85fe
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-02-09 15:05:57 +0100

    Add spec and setup codegen

---
+++ js/NativeOnfidoModule.ts
@@ -0,0 +1,7 @@
+import { TurboModuleRegistry, TurboModule } from "react-native";
+
+export interface Spec extends TurboModule {
+  start(config: Object): Promise<string>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>("OnfidoSdk");
