29027a63d7d8394517b2d79d4e98fb1dc8ed8ccd
LeeviKopakkala <leevi.kopakkala@hotmail.com>
2024-02-22 17:25:19 +0200

    Fix for required flowSteps if workflowRunId exists.

--- js/config_constants.ts
+++ js/config_constants.ts
@@ -30,8 +30,6 @@ export type OnfidoResult = {
 
 export type OnfidoConfig = {
   sdkToken: string;
-  workflowRunId?: string;
-  flowSteps: OnfidoFlowSteps;
   hideLogo?: boolean;
   logoCoBrand?: boolean;
   disableNFC?: boolean;
@@ -40,7 +38,10 @@ export type OnfidoConfig = {
     ios_strings_file_name?: string;
   };
   theme: OnfidoTheme;
-};
+} & (
+  | { workflowRunId: undefined; flowSteps: OnfidoFlowSteps }
+  | { workflowRunId: string; flowSteps?: OnfidoFlowSteps }
+);
 
 export interface OnfidoError extends Error {
   code?: string;
